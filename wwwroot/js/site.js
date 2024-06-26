f0bf1ce60e8617b408a0e189672ce33c2f306fd7
Abdul Muqaddim <118798028+Muqaddim608@users.noreply.github.com>
2024-06-26 18:08:05 +0500

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿var slideIndex = 0;
+var timer;
+
+function startSlide() {
+    timer = setInterval(() => {
+        slideIndex++;
+        if (slideIndex >= $('.slide').length) {
+            slideIndex = 0;
+        }
+        showSlides();
+    }, 5000); // Change image every 5 seconds
+}
+
+function showSlides() {
+    $('.slides').css('transform', `translateX(${-slideIndex * 100}%)`);
+}
+
+$(document).ready(function () {
+    startSlide();
+});
+function scrollToSection(brand) {
+    var element = document.getElementById(brand);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+}
